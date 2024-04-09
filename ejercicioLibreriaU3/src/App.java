5bd72fff0c4c804cdefab12953c48665cd4e22de
Alfredo Aguilar <yoaguilar78@gmail.com>
2024-04-09 17:50:31 -0600

    CLASE DIA 9 ABRIL

---
+++ ejercicioLibreriaU3/src/App.java
@@ -0,0 +1,10 @@
+import Usuarios.Cliente;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Cliente clientePrueba = new Cliente ("Juan", "Rivera", "4432344234");
+        System.out.println(clientePrueba.toString());
+    }
+    }
+
---
+++ ejercicioLibreriaU3/src/Utils/Rol.java
@@ -0,0 +1,7 @@
+package Utils;
+
+public enum Rol {
+    CLIENTE,
+    ASISTENTE,
+    GERENTE
+}
